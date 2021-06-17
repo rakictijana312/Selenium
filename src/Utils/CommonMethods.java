ee9791b6d5e5498672b317c1e5f1b492580e404e
rakictijana312 <rakictijana312@gmail.com>
2021-06-17 19:34:07 -0400

    first commit

--- src/Utils/CommonMethods.java
+++ src/Utils/CommonMethods.java
@@ -1,19 +1,19 @@
-package Utils;
+//package Utils;
 
-import SeleniumClass12.TestBase.BaseClass;
-import TestBase.BaseClass;
+//import SeleniumClass12.TestBase.BaseClass;
+//import TestBase.BaseClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class CommonMethods extends BaseClass {
-//dakle kad stavimo static ne treba da kreiramo object
-   public static void  sendText(WebElement element, String text){
-      element.clear();
-      element.sendKeys(text);
-
-
-   }
-}
+//public class CommonMethods extends BaseClass {
+////dakle kad stavimo static ne treba da kreiramo object
+//   public static void  sendText(WebElement element, String text){
+//      element.clear();
+//      element.sendKeys(text);
+//
+//
+//   }
+//}
